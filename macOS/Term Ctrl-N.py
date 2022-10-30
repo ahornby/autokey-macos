8c6a63ec839f7f0ea9fcb5777b5b434aed0c064c
Alex Hornby <alex@hornby.org.uk>
2022-10-30 17:15:58 +0000

    map command to ctrl except for in terminal

--- macOS/Term Ctrl-N.py
+++ macOS/Term Ctrl-N.py
@@ -1,5 +1,5 @@
 wcl = window.get_active_class()
 if not (wcl == "gnome-terminal-server.Gnome-terminal" or wcl == "konsole.konsole"):
-    keyboard.send_keys("<ctrl>+p")
+    keyboard.send_keys("<ctrl>+n")
 else:
-    keyboard.send_keys("<ctrl>+p")
+    keyboard.send_keys("<ctrl>+n")
--- macOS/Term Suspend Undo.py
+++ macOS/Term Suspend Undo.py
@@ -1,3 +1,5 @@
 wcl = window.get_active_class()
 if not (wcl == "gnome-terminal-server.Gnome-terminal" or wcl == "konsole.konsole"):
     keyboard.send_keys("<ctrl>+z")
+else:
+    keyboard.send_keys("<ctrl>+z")
